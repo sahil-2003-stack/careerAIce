060437031a681896e2dff84f4098a38f44f018ea
Sahil Soni <sonisahil2001@gmail.com>
2025-03-14 07:27:34 +0530

    Delete jhatu directory

--- jhatu/app/(auth)/layout.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Authlayout = ({children}) => {
-    return <div className='flex justify-center pt-40 pb-20'>{children}</div>;
-};
-
-
-export default Authlayout;
--- jhatu/lib/inngest/functions.js
+++
@@ -1,3 +0,0 @@
-import { inngest } from "./client";
-
-
